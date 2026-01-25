3d0af67275683db1ae5ab33292b3b91b6f82cfd0
Maximilian Thur <maxthur95@gmail.com>
2026-01-25 17:50:37 +0100

    uv install

--- src/fetcher/fetch_data/helpers.py
+++ src/fetcher/fetch_data/helpers.py
@@ -3,6 +3,6 @@ import subprocess
 
 def ensure_latest_pkg(pkg: str):
     subprocess.run(
-        ["python", "-m", "pip", "install", "-U", pkg],
+        ["uv", "pip", "install", "-U", pkg],
         check=True,
     )
